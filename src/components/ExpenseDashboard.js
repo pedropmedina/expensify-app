ac2a1746e68c19326cf63d40f74ee9757ba1884c
pedropmedina <pedrodelilia@gmail.com>
2018-06-11 01:59:39 -0400

    Add expense summary to dashboard

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 class ExpenseDashboard extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
+				<ExpensesSummary />
 				<ExpenseListFilters />
 				<ExpenseList />
 			</React.Fragment>
---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+	const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+	return (
+		<div>
+			<h2>
+				Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
+			</h2>
+		</div>
+	);
+};
+
+const mapStateToProps = ({ expenses, filters }) => {
+	const visibleExpenses = selectExpenses(expenses, filters);
+
+	return {
+		expenseCount: visibleExpenses.length,
+		expensesTotal: selectExpensesTotal(visibleExpenses),
+	};
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
