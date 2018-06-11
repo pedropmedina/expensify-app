aba85d5e4ff8228216b93d7f482aff7a68435f4e
pedropmedina <pedrodelilia@gmail.com>
2018-06-10 22:50:29 -0400

    Setup production build and package.json for deployment to heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+const distPath = path.join(__dirname, '..', 'dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(distPath, 'index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`App served on port ${port}`);
